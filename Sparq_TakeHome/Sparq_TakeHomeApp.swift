faf80ef0c3dff9f0328eed0590e6680469c9a271
cmillstein1 <cmillstein1@me.com>
2026-01-20 19:48:12 -0500

    Initial Commit

---
+++ Sparq_TakeHome/Sparq_TakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sparq_TakeHomeApp.swift
+//  Sparq_TakeHome
+//
+//  Created by Casey Millstein on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Sparq_TakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
